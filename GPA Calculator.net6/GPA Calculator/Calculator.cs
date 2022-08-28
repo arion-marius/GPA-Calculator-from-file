91d3a1c685b12340dc4ab404a676115aef628de6
Arion Marius <arion.marius53@yahoo.com>
2022-08-28 17:46:49 +0300

    Calculator

---
+++ GPA Calculator.net6/GPA Calculator/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GPA_Calculator
+{
+    public static class Calculator
+    {
+        public static double GetMedie(int[] grades, int? thesis)
+        {
+            var sum = 0d;
+            for (int i = 0; i < grades.Length; i++)
+                sum += grades[i];
+
+            var medie = sum / grades.Length;
+
+            medie = thesis == null
+                ? medie
+                : (medie * 3 + thesis.Value) / 4;
+
+            return Math.Round(medie, 2);
+
+            //return thesis == null
+            //    ? Math.Round(sum / grades.Length, 2)
+            //    : Math.Round((sum / grades.Length * 3 + thesis.Value) / 4, 2);
+        }
+    }
+}
---
+++ GPA Calculator.net6/GPA Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace GPA_Calculator
+{
+    internal static class Program
+    {
+        private const char LineElementsSeparator = '|';
+        private const char GradesSeparator = ',';
+
+        static void Main(string[] args)
+        {
+            var content = File.ReadAllText(@"C:\Users\arion\Desktop\GPA\GPA-Calculator-from-file\grades.txt");
+            string[] lines = GetLinii(content);
+
+            foreach (var line in lines)
+            {
+                string[] elements = GetElements(line);
+                var subjectName = elements[0];
+                int[] grades = GetGrades(elements[1]);
+                int? thesis = null;
+                if (elements.Length > 2)
+                    thesis = GetThesis(elements[2]);
+
+                double medie = Calculator.GetMedie(grades, thesis);
+                Console.WriteLine($"Media ta la {subjectName} este:{medie}");
+            }
+        }
+
+        private static string[] GetLinii(string content)
+            => content.Split(Environment.NewLine);
+
+        private static string[] GetElements(string line)
+            => line.Split(LineElementsSeparator);
+
+        private static int GetThesis(string v)
+            => Convert.ToInt32(v);
+
+        private static int[] GetGrades(string v)
+        {
+            string[] splitGrades = v.Split(GradesSeparator);
+            int[] ints = new int[splitGrades.Length];
+            for (int i = 0; i < splitGrades.Length; i++)
+            {
+                ints[i] = Convert.ToInt32(splitGrades[i]);
+            }
+            return ints;
+        }
+    }
+}
