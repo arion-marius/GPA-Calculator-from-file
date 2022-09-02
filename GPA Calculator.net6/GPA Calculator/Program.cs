bb5912a11b9eb3529e628ad7507ac64d3229b5cb
Arion Marius <arion.marius53@yahoo.com>
2022-09-02 18:12:43 +0300

    Calculator

--- GPA Calculator.net6/GPA Calculator/Program.cs
+++ GPA Calculator.net6/GPA Calculator/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GPA_Calculator
 {
@@ -10,41 +15,57 @@ namespace GPA_Calculator
 
         static void Main(string[] args)
         {
-            var content = File.ReadAllText(@"C:\Users\arion\Desktop\GPA\GPA-Calculator-from-file\grades.txt");
-            string[] lines = GetLinii(content);
+            string folder = "C:\\Users\\arion\\Desktop\\GPA\\GPA-Calculator-from-file\\Grades";
+            int b = 0;
+            var files = Directory.EnumerateFiles(folder, "*.txt");
+            string[] text = new string[files.Count()];
 
-            foreach (var line in lines)
+            foreach (string file in files)
             {
-                string[] elements = GetElements(line);
-                var subjectName = elements[0];
-                int[] grades = GetGrades(elements[1]);
-                int? thesis = null;
-                if (elements.Length > 2)
-                    thesis = GetThesis(elements[2]);
+                double sumaMedii = 0;
+                string numeElev = Path.GetFileName(file);
+                string contents = File.ReadAllText(file);
+                string[] lines = GetLinii(contents);
 
-                double medie = Calculator.GetMedie(grades, thesis);
-                Console.WriteLine($"Media ta la {subjectName} este:{medie}");
+                foreach (var line in lines)
+                {
+                    string[] elements = GetElements(line);
+                    int[] grades = GetGrades(elements[1]);
+                    int? thesis = null;
+                    if (elements.Length > 2)
+                        thesis = GetThesis(elements[2]);
+                    double medie = Calculator.GetMedie(grades, thesis);
+                    sumaMedii += medie;
+                }
+
+                var medieGeneralaFinala = Math.Round(sumaMedii / lines.Length, 2);
+                text[b++] = $"Media elevului/elevei {numeElev} este {medieGeneralaFinala}";
             }
-        }
+            var fisierMedii = @"C:\\Users\\arion\\Desktop\\GPA\\GPA-Calculator-from-file\\medii.txt";
+            File.AppendAllLines(fisierMedii, text);
 
-        private static string[] GetLinii(string content)
-            => content.Split(Environment.NewLine);
 
-        private static string[] GetElements(string line)
-            => line.Split(LineElementsSeparator);
 
-        private static int GetThesis(string v)
-            => Convert.ToInt32(v);
+            static string[] GetLinii(string content)
+               => content.Split(Environment.NewLine);
 
-        private static int[] GetGrades(string v)
-        {
-            string[] splitGrades = v.Split(GradesSeparator);
-            int[] ints = new int[splitGrades.Length];
-            for (int i = 0; i < splitGrades.Length; i++)
+            static string[] GetElements(string line)
+                => line.Split(LineElementsSeparator);
+
+            static int GetThesis(string v)
+                => Convert.ToInt32(v);
+
+            static int[] GetGrades(string v)
             {
-                ints[i] = Convert.ToInt32(splitGrades[i]);
+                string[] splitGrades = v.Split(GradesSeparator);
+                int[] ints = new int[splitGrades.Length];
+                for (int i = 0; i < splitGrades.Length; i++)
+                {
+                    ints[i] = Convert.ToInt32(splitGrades[i]);
+                }
+                return ints;
             }
-            return ints;
         }
     }
+
 }
